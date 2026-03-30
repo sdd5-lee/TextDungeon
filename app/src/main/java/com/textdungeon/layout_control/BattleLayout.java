2c47a776304abedb289820b8e151bfdb7d0581e8
sdd5 <rudwo6285@gmail.com>
2026-03-30 23:11:13 +0900

    배틀 엑티비티에 테스트용 버튼들 추가 배틀레이아웃에서 저장되는 방법이랑 로드되는 것 확인 build.gradle에 json을 읽어주는Gson 라이브러리 추가 게임 세이브 클래스 추가 플레이어,현재 점수, 층수를 저장하고 로드하는 기능 슬롯 넘버로 플레이어를 구분함 player 클래스에 인벤토리 추가 인벤토리는 아이템 이름만 저장하고 추후에 아이템을 이름으로 찾아서 적용하는 식으로 동작할 예정 스탯 레이아웃 관리를 위한 클래스 추가

--- app/src/main/java/com/textdungeon/layout_control/BattleLayout.java
+++ app/src/main/java/com/textdungeon/layout_control/BattleLayout.java
@@ -3,18 +3,66 @@ package com.textdungeon.layout_control;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.textdungeon.MainActivity;
 import com.example.textdungeon.R;
+import com.textdungeon.player.Player;
+import com.textdungeon.system.GameSave;
 
 public class BattleLayout extends AppCompatActivity {
-
+    GameSave gameSave = new GameSave(1,new Player("테스트용"));
+    Player player = gameSave.getPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.battle_activity);
+
+        TextView textView = findViewById(R.id.inv);
+        Button testbutton = findViewById(R.id.test);
+        testbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                player.pickUpItem("임시아이템");
+                for (String s : player.getInventory()) {
+                    if (player.getInventory().isEmpty()) {
+                        textView.setText("인벤토리 : "+s);
+                    }
+                    textView.setText(textView.getText()+","+s);
+                }
+            }
+        });
+
+
+        Button save = findViewById(R.id.savebutton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BattleLayout.this, "저장되었습니다 ", Toast.LENGTH_SHORT).show();
+                gameSave.save(BattleLayout.this);
+            }
+        });
+        Button load = findViewById(R.id.loadbutton);
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BattleLayout.this, "게임을 불러왔습니다", Toast.LENGTH_SHORT).show();
+                gameSave = GameSave.load(BattleLayout.this, 1);
+                if (gameSave != null) {
+                    player = gameSave.getPlayer(); // 플레이어 객체도 동기화
+                }
+                for (String s : player.getInventory()) {
+                    if (player.getInventory().isEmpty()) {
+                        textView.setText("인벤토리 : "+s);
+                    }
+                    textView.setText(textView.getText()+","+s);
+                }
+            }
+        });
     }
     public void battleMoveMain(View v){
         Intent intent = new Intent(BattleLayout.this, MainActivity.class);
